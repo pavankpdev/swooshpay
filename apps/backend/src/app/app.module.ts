7459b7c2cf000f56bf1c16465d8b29c138ead35a
pavankpdev <pavankumarkpkp@gmail.com>
2025-08-28 15:09:05 +0530

    fix(auth): remove global jwt guard
    This global guard was overriding the verify guard

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
@@ -14,12 +12,6 @@ import { AuthModule } from '../auth/auth.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: JwtAuthGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
